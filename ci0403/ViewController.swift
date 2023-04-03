3baf2de65085b122d4f5ac9070f8d4091030c41c
wei.zhx <charles_wei_1991@126.com>
2023-04-03 16:14:35 +0800

    no message

--- ci0403/ViewController.swift
+++ ci0403/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("裂开...")
         // Do any additional setup after loading the view.
     }
 
